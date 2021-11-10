40e8977f4d76bb9f16578a16b053a5124144f70e
IsaGuapo <isabela.guapo@gmail.com>
2021-11-10 11:07:48 +0100

    realizada conexion mongoose

--- prueba.js
+++ prueba.js
@@ -10,74 +10,104 @@ mongoose.connect(url, function (err) {
 });
 
 
-///////////////////////// creacion de la estructura de la BBDD ////////////////////////
-const objectSchema = {
+///////////////////////// creacion de la estructura de la BBDD TABLA 1  JUGADOR////////////////////////
+const objectSchemaJugador = {
     _id: mongoose.Schema.Types.ObjectId,
-        nombre: String,
-        email: String,
-        fecha: Date,
-      
-    
-       
-};
-const objectSchema2 = {
-    _id: mongoose.Schema.Types.ObjectId,
-        partidas_jugadas: Number,
-        puntuacion: Number,
-        preguntas:{
-            q_correctas: String,
-            q_incorrectas: String,
-        }
-
-};
-
-/*
-//////////////////////////// Insercion de datos en el schema ///////////////////////
-
-const userSchema = mongoose.Schema(objectSchema, objectSchema2);
-
-
-let User = mongoose.model('quiz1', userSchema,objectSchema2); //quiz1 corresponde al nombre de la coleccion
-
-/*let dataUser = {
-    _id: new mongoose.Types.ObjectId(),
-    nombre: "Jalid",
-    fecha: Date.now(),
-    puntuacion: 10,
-    partidas_jugadas: 10,
-    preguntas:{
-        q_correctas: "1,2,3,4,5,6,7,8,9,10",
-        q_incorrectas: "0"
+    nombre: String,
+    email: {
+        type: String,
+        match: [/.+\@.+\..+/, "Por favor ingrese un correo válido"],
+        password: {
+            type: String,
+            validate: {
+                validator: function (pass) {
+                    return (pass.length >= 6);
+                },
+                message: "La contraseña debe tener al menos 6 caracteres"
+            }
+        },
     }
-    
-
 };
-let userPartidas = new User(dataUser);
-userPartidas.save(function (err) {
+//// genero vaiable para crear dentro la coleccion //////
+const userSchema = mongoose.Schema(objectSchemaJugador);
+/*
+const Player = mongoose.model('Player', userSchema); //quiz1 corresponde al nombre de la coleccion
+/////// Insercion de datos /////////////
+let dataUser = {
+    _id: new mongoose.Types.ObjectId(),
+    nombre: "Mario",
+    email: "mma@gmail.com",
+    password: "125478ll",
+    };
+
+///////// guardado de los datos /////////
+let user = new Player(dataUser);
+user.save(function (err) {
     if (err) throw err;
     console.log("Inserción correcta");
     mongoose.disconnect();
 });
+*/
+//////////////// CREACION DE SEGUNDA TABLA PARTIDAS ///////////////
 
+const objectSchemaPartidas = {
+    _id: mongoose.Schema.Types.ObjectId,
+    id_jugador: { type: mongoose.Schema.ObjectId, ref: "Quiz1" },// relaciona tablas
+    fecha: Date, 
+    partidas_jugadas: Number,
+    puntuacion: Number,
+    preguntas: {
+        q_correctas: String,
+        q_incorrectas: String,
+    }
+};
+
+///// varibale para partidas /////////////
+const partidasSchema = mongoose.Schema(objectSchemaPartidas);
+/*
+////// Insercion de 2º tabla /////////
+const Game = mongoose.model('Game', partidasSchema);
+let Partidas = {
+    _id: new mongoose.Types.ObjectId(),
+    id_jugador: "618b953438adeaba7a57cad2",
+    fecha:Date.now(),
+    puntuacion: 10,
+    partidas_jugadas: 10,
+    preguntas: {
+        q_correctas: "1,2,3,4,5,6,7,8,9,10",
+        q_incorrectas: "0"
+    }
+};
+let dataPartidas = new Game(Partidas);
+
+    dataPartidas.save(function (err) {
+        if (err) throw err;
+        console.log("Inserción correcta");
+        mongoose.disconnect();
+    });
+
+*/
+
+/*
 ///////////////// Actualizacion ///////////////////////////////////
 
-User.findById('61893e4fbe5fad50600a435a', function(err, user){
+Player.findById('618b953438adeaba7a57cad2', function (err, user) {
     if (err) throw err;
     user.partidas_jugadas = 5;
-    user.save(function(err){
+    user.save(function (err) {
         if (err) throw err;
         console.log("Actualización correcta");
         mongoose.disconnect();
     });
-    
+
 });
-*/
+
 /*
 /////////// busqueda  Obtener el ranking por participante y día //////////////////////////////////
 
 User.findById('61893e4fbe5fad50600a435a', function(err, user){
     if (err) throw err;
-    console.log("User con id 61893e4fbe5fad50600a435a tiene una puntuación de :\n"+user.puntuacion+":\n" 
+    console.log("User con id 61893e4fbe5fad50600a435a tiene una puntuación de :\n"+user.puntuacion+":\n"
     +"y las respuestas acertadas son " +user.preguntas.q_correctas);
     mongoose.disconnect();
 });
